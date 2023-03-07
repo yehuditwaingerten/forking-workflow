095c0f5c070459d52da483126429f9072ea5dae4
Francis Navarro <lnavarro@hnu.edu.ph>
2023-03-07 10:08:21 +0800

    Initial Commit

---
+++ forking-workflow.c
@@ -0,0 +1,19 @@
+/*
+	Activity Name:	Forking Workflow
+	Description:	This activity will allow students to try forking from a public GitHub Repository.
+*/
+#include <stdio.h>
+
+int main(){
+	printf("Forking workflow activity\n");
+	printf("Your name must be printed if this program is run. \n\n");
+	
+	// Call your function below this line.
+	
+	return 0;
+}
+
+
+// Refer to the function name for the naming of your funcion.
+void navarro_printName() {
+}
